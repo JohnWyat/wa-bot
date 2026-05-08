0951fdd6983229dbebdf7cd702cb3c8441a629f3
JohnWyat <142235406+JohnWyat@users.noreply.github.com>
2026-05-08 11:27:58 +0300

    added home route

--- server.js
+++ server.js
@@ -55,6 +55,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+    res.send("WhatsApp bot running");
+});
+
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
